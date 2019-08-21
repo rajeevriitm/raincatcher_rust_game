e74f46691c831f701517360ebea2773ccf3c286a
rajeev <rajeevriitm@gmail.com>
2019-08-21 18:54:57 +0530

    basic animations implemneted

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 // use crate::model::Draw;
+use std::cell::RefCell;
+use std::rc::Rc;
+use wasm_bindgen::JsCast;
+
 use model::*;
 use wasm_bindgen::prelude::*;
 mod model;
@@ -6,23 +10,78 @@ use model::{RainDrop, World};
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-const DROP_VELOCITY: f64 = 12.0;
-use std::time::Instant;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-    let world = World::new("game-of-drops");
-    let mut drop = RainDrop::new();
-    let bucket = Bucket::new(&world);
-    let timer = Instant::now();
-    drop.draw(&world);
-    bucket.draw(&world);
-    loop {
-        drop.move_to_point(drop.centre_y + DROP_VELOCITY * (timer.elapsed().as_secs() as f64));
-        drop.draw(&world);
-    }
+    let mut world = World::new("game-of-drops");
+    // let mut drop = RainDrop::new(100.0);
+    // let bucket = Bucket::new(&world);
+    // let mut timer = Timer::new();
+    // let i: i32 = drop.centre_y.into();
+    // drop.draw(&world.canvas);
+    // world.add_new_drop();
+    // world.remove_drop();
+    let closure_cell = Rc::new(RefCell::new(None));
+    let closure_cell_clone = Rc::clone(&closure_cell);
+    // let bucket = Rc::new(RefCell::new(bucket));
+    let bucket_clone_keydown = Rc::clone(&world.bucket);
+    let bucket_clone_keyup = Rc::clone(&world.bucket);
+    *closure_cell.borrow_mut() = Some(Closure::wrap(Box::new(move |x: f64| {
+        web_sys::console::log_1(&x.into());
+        // world.clear_canvas();
+        world.add_new_drop();
+        world.check_collision();
+        world.animation_cycle();
+        // bucket.borrow().draw(&world.canvas);
+        // drop.move_to_point(drop.centre_y + DROP_VELOCITY * (timer.time_elapsed()));
+        // drop.draw(&world.canvas);
+        request_animation_frame(closure_cell_clone.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut(_)>));
+    request_animation_frame(closure_cell.borrow().as_ref().unwrap());
+    let keydown_closure = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+        if event.key() == "ArrowRight" {
+            bucket_clone_keydown.borrow_mut().direction = Direction::Right;
+        // bucket_clone.borrow_mut().centre_x += 10.0;
+        } else if event.key() == "ArrowLeft" {
+            bucket_clone_keydown.borrow_mut().direction = Direction::Left;
+            // bucket_clone.borrow_mut().centre_x -= 10.0;
+        }
+    }) as Box<dyn FnMut(_)>);
+    let keyup_closure = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+        let key = event.key();
+        let direction = bucket_clone_keyup.borrow().direction;
+        if (key == "ArrowRight" && direction == Direction::Right)
+            || (key == "ArrowLeft" && direction == Direction::Left)
+        {
+            bucket_clone_keyup.borrow_mut().direction = Direction::None;
+        }
+    }) as Box<dyn FnMut(_)>);
+    request_keydown_event(&keydown_closure);
+    request_keyup_event(&keyup_closure);
+    keydown_closure.forget();
+    keyup_closure.forget();
 
     Ok(())
+}
+fn request_animation_frame(f: &Closure<dyn FnMut(f64)>) {
+    web_sys::window()
+        .unwrap()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+fn request_keydown_event(f: &Closure<dyn FnMut(web_sys::KeyboardEvent)>) {
+    web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .set_onkeydown(Some(f.as_ref().unchecked_ref()))
+}
+fn request_keyup_event(f: &Closure<dyn FnMut(web_sys::KeyboardEvent)>) {
+    web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .set_onkeyup(Some(f.as_ref().unchecked_ref()))
 }
--- src/model.rs
+++ src/model.rs
@@ -1,9 +1,24 @@
+#![feature(euclidean_division)]
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::JsCast;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 use Color::*;
-const BUCKET_HEIGHT: f64 = 15.0;
-const BUCKET_WIDTH: f64 = 10.0;
-const BUCKET_SLAND: f64 = 5.0;
+const BUCKET_HEIGHT: f64 = 30.0;
+const BUCKET_BOTTOM_WIDTH: f64 = 30.0;
+const BUCKET_SLAND: f64 = 15.0;
+pub const DROP_VELOCITY: f64 = 0.8;
+const DROP_DISTANCE: f64 = 80.0;
+pub const BUCKET_SPEED: f64 = 2.5;
+const DROP_INDENT: f64 = 5.0;
+const BUCKET_WIDTH: f64 = BUCKET_BOTTOM_WIDTH + 2.0 * BUCKET_SLAND;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Direction {
+    Right = 1,
+    Left = -1,
+    None = 0,
+}
 #[derive(Debug)]
 enum Color {
     Black,
@@ -22,25 +37,110 @@ impl Color {
 
 #[derive(Debug)]
 pub struct World {
-    canvas: HtmlCanvasElement,
+    pub canvas: CanvasRenderingContext2d,
+    raindrops: Vec<RainDrop>,
+    pub bucket: Rc<RefCell<Bucket>>,
     height: f64,
     width: f64,
+    timer: Timer,
+    // score: u32,
 }
 impl World {
     pub fn new(selector: &str) -> World {
         let document = web_sys::window().unwrap().document().unwrap();
         let canvas = document.get_element_by_id(selector).unwrap();
-        let canvas: web_sys::HtmlCanvasElement = canvas
+        let width = canvas.scroll_width();
+        assert!((DROP_DISTANCE / DROP_VELOCITY) * BUCKET_SPEED < width.into());
+        let height = canvas.scroll_height();
+        let canvas: CanvasRenderingContext2d = canvas
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .map_err(|_| ())
+            .unwrap()
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()
             .unwrap();
-        let width = canvas.scroll_width();
-        let height = canvas.scroll_height();
+        let bucket = Bucket::new(height as f64 - BUCKET_HEIGHT);
+        let bucket = Rc::new(RefCell::new(bucket));
         World {
             canvas,
             height: height.into(),
             width: width.into(),
+            raindrops: vec![],
+            timer: Timer::new(),
+            bucket,
         }
+    }
+    pub fn clear_canvas(&self) {
+        self.canvas.clear_rect(0.0, 0.0, self.width, self.height);
+    }
+    pub fn add_new_drop(&mut self) {
+        match self.raindrops.last() {
+            Some(raindrop) => {
+                if raindrop.centre_y > DROP_DISTANCE {
+                    let delta_x = self.get_random_distance();
+                    // web_sys::console::log_1(&delta_x.into());
+                    // random plus or minus
+                    let x = if (delta_x as i64) % 2 == 0 {
+                        // (raindrop.centre_x + delta_x) % self.width
+                        limit_values(
+                            raindrop.centre_x + delta_x,
+                            raindrop.radius + 20.0,
+                            self.width - 20.0,
+                        )
+                    } else {
+                        limit_values(
+                            raindrop.centre_x - delta_x,
+                            raindrop.radius + 10.0,
+                            self.width - 10.0,
+                        )
+                        // (self.width + raindrop.centre_x - delta_x) % self.width
+                    };
+                    self.raindrops.push(RainDrop::new(x));
+                }
+            }
+            None => {
+                self.raindrops
+                    .push(RainDrop::new(self.get_random_distance() % self.width));
+            }
+        }
+    }
+    pub fn check_collision(&mut self) {
+        if let Some(raindrop) = self.raindrops.first() {
+            let y = raindrop.centre_y;
+            let x = raindrop.centre_x;
+            if y > self.height - BUCKET_HEIGHT {
+                let bucket_x = self.bucket.borrow().centre_x;
+                if x > bucket_x && x < bucket_x + BUCKET_WIDTH {
+                    self.raindrops.remove(0);
+                } else if y > self.height - BUCKET_HEIGHT / 2.0 {
+                    //lost
+                }
+            }
+        }
+    }
+    fn get_random_distance(&self) -> f64 {
+        let value = js_sys::Math::random() * (DROP_DISTANCE / DROP_VELOCITY) * BUCKET_SPEED;
+        // web_sys::console::log_1(&value.into());
+        f64::min(self.width, value)
+    }
+    pub fn animation_cycle(&mut self) {
+        self.clear_canvas();
+        let time_elapsed = self.timer.time_elapsed();
+        // web_sys::console::log_1(&(self.raindrops.len() as u32).into());
+        for raindrop in self.raindrops.iter_mut() {
+            raindrop.move_to_point(raindrop.centre_y + DROP_VELOCITY * time_elapsed);
+            raindrop.draw(&self.canvas);
+        }
+        let mut bucket = self.bucket.borrow_mut();
+        let to_point = limit_values(
+            bucket.centre_x + bucket.direction as i64 as f64 * BUCKET_SPEED * time_elapsed,
+            -BUCKET_WIDTH,
+            self.width,
+        );
+        bucket.move_to_point(to_point);
+        bucket.draw(&self.canvas);
     }
 }
 #[derive(Debug)]
@@ -50,10 +150,10 @@ pub struct RainDrop {
     pub centre_y: f64,
 }
 impl RainDrop {
-    pub fn new() -> RainDrop {
+    pub fn new(x: f64) -> RainDrop {
         RainDrop {
-            centre_y: 100.0,
-            centre_x: 100.0,
+            centre_y: -10.0,
+            centre_x: x,
             radius: 10.0,
         }
     }
@@ -66,25 +166,30 @@ pub struct Bomb {
 }
 #[derive(Debug)]
 pub struct Bucket {
-    centre_x: f64,
+    pub centre_x: f64,
     centre_y: f64,
+    pub direction: Direction,
 }
 impl Bucket {
-    pub fn new(world: &World) -> Bucket {
+    pub fn new(centre_y: f64) -> Bucket {
         Bucket {
-            centre_y: world.height - BUCKET_HEIGHT,
+            centre_y: centre_y,
             centre_x: 20.0,
+            direction: Direction::None,
         }
+    }
+    pub fn move_to_point(&mut self, x: f64) {
+        self.centre_x = x;
     }
 }
 pub trait Draw {
-    fn draw(&self, world: &World);
+    fn draw(&self, htmlCanvas: &CanvasRenderingContext2d);
     fn move_to_point(&mut self, to_point: f64);
 }
 impl Draw for RainDrop {
-    fn draw(&self, world: &World) {
-        let context = get_canvas_context(&world);
-        context
+    fn draw(&self, htmlCanvas: &CanvasRenderingContext2d) {
+        htmlCanvas.begin_path();
+        htmlCanvas
             .arc(
                 self.centre_x,
                 self.centre_y,
@@ -93,8 +198,8 @@ impl Draw for RainDrop {
                 std::f64::consts::PI * 2.0,
             )
             .unwrap();
-        context.set_fill_style(&Black.get_rgb().into());
-        context.fill();
+        htmlCanvas.set_fill_style(&Black.get_rgb().into());
+        htmlCanvas.fill();
         // context.set_fill_style(2);
     }
     fn move_to_point(&mut self, to_point: f64) {
@@ -102,33 +207,43 @@ impl Draw for RainDrop {
     }
 }
 impl Draw for Bucket {
-    fn draw(&self, world: &World) {
-        let context = get_canvas_context(&world);
-        context.move_to(self.centre_x.into(), self.centre_y);
-        context.line_to(self.centre_x + BUCKET_SLAND, self.centre_y + BUCKET_HEIGHT);
-        context.line_to(
-            self.centre_x + BUCKET_SLAND + BUCKET_WIDTH,
+    fn draw(&self, htmlCanvas: &CanvasRenderingContext2d) {
+        htmlCanvas.begin_path();
+        htmlCanvas.move_to(self.centre_x.into(), self.centre_y);
+        htmlCanvas.line_to(self.centre_x + BUCKET_SLAND, self.centre_y + BUCKET_HEIGHT);
+        htmlCanvas.line_to(
+            self.centre_x + BUCKET_SLAND + BUCKET_BOTTOM_WIDTH,
             self.centre_y + BUCKET_HEIGHT,
         );
-        context.line_to(
-            self.centre_x + 2.0 * BUCKET_SLAND + BUCKET_WIDTH,
+        htmlCanvas.line_to(
+            self.centre_x + 2.0 * BUCKET_SLAND + BUCKET_BOTTOM_WIDTH,
             self.centre_y,
         );
-        context.set_fill_style(&Black.get_rgb().into());
-        context.fill();
+        htmlCanvas.set_fill_style(&Black.get_rgb().into());
+        htmlCanvas.fill();
     }
     fn move_to_point(&mut self, to_point: f64) {
         self.centre_x = to_point;
     }
 }
-fn get_canvas_context(world: &World) -> CanvasRenderingContext2d {
-    let context = world
-        .canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-    context.begin_path();
-    context
+#[derive(Debug)]
+pub struct Timer {
+    start_time: f64,
+}
+impl Timer {
+    pub fn new() -> Timer {
+        let start_time = web_sys::window().unwrap().performance().unwrap().now();
+        Timer { start_time }
+    }
+    pub fn time_elapsed(&mut self) -> f64 {
+        let time_now = web_sys::window().unwrap().performance().unwrap().now();
+        let time_elapsed = (time_now - self.start_time) / 5.0;
+        self.start_time = time_now;
+        time_elapsed
+    }
+}
+fn limit_values(value: f64, min: f64, max: f64) -> f64 {
+    // value.rem_euclid(max - min)
+    ((value + max - 2.0 * min) % (max - min)) + min
+    // f64::max(min, (value + max) % max)
 }
